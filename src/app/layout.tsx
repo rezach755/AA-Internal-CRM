90aeed2c62f02da8d6936d0a3713807eb47f47f9
rezach755 <reza.ch755@gmail.com>
2024-11-12 17:27:05 +0330

    setting farsi font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { Layout } from "@/components";
-
+import localFont from "next/font/local";
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700", "900"],
@@ -15,13 +15,27 @@ export const metadata: Metadata = {
     "Introducing Tailwind Course Landing Page, a versatile and engaging landing page template designed using Tailwind CSS and Material Tailwind.",
 };
 
+const yekan = localFont({
+  src: [
+    {
+      path: "../fonts/BYekan.ttf",
+      weight: "400",
+    },
+    {
+      path: "../fonts/BYekanBold.ttf",
+      weight: "700",
+    },
+  ],
+  variable: "--font-yekan",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html dir="rtl" lang="fa">
+    <html dir="rtl" lang="fa" className={`${yekan.variable}`}>
       <head>
         <script
           defer
@@ -30,7 +44,7 @@ export default function RootLayout({
         ></script>
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </head>
-      <body className={`${roboto.className}`}>
+      <body className={`${roboto.className} ${yekan.variable} `}>
         <Layout>
           {children}
           {/* <FixedPlugin /> */}
