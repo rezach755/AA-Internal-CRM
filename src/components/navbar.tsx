7ba1d41cf4c8b479dcdc9fe70f3106b77316a189
rezach755 <reza.ch755@gmail.com>
2024-11-19 08:34:09 +0330

    set active section and set motion

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import {
   Navbar as MTNavbar,
   Collapse,
@@ -15,33 +16,35 @@ import {
 //   Bars3Icon,
 // } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import clsx from "clsx";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 const additionalProps = {
   placeholder: "",
-  onPointerEnterCapture: () => {},
-  onPointerLeaveCapture: () => {},
+  // onPointerEnterCapture: () => {},
+  // onPointerLeaveCapture: () => {},
 };
 
-const NAV_MENU = [
+export const NAV_MENU = [
   {
     name: "خانه",
     hash: "#home",
   },
   {
-    name: "درباره ما",
-    hash: "#about",
+    name: "آمار ها",
+    hash: "#stats",
   },
   {
     name: "پروژه ها",
     hash: "#projects",
   },
   {
-    name: "تیم آرش و آرین",
-    hash: "#skills",
+    name: "اخذ نمایندگی",
+    hash: "#representation",
   },
   {
-    name: "فرصت ها",
-    hash: "#experience",
+    name: "درباره ما",
+    hash: "#about",
   },
   {
     name: "تماس با ما",
@@ -54,20 +57,48 @@ interface NavItemProps {
   href?: string;
 }
 
-function NavItem({ children, href }: NavItemProps) {
+function NavItem({}: NavItemProps) {
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
   return (
-    <li>
-      <Typography
-        {...additionalProps}
-        as="a"
-        href={href || "#"}
-        target={href ? "_blank" : "_self"}
-        variant="paragraph"
-        className="flex items-center gap-2 font-medium text-[#062650] "
-      >
-        {children}
-      </Typography>
-    </li>
+    <ul className="flex w-[50rem] flex-wrap item-center justify-center whitespace-nowrap gap-y-1 text-[0.9rem] font-medium text-[#062650] sm:w-[initial] sm:flex-nowrap sm:gap-5">
+      {NAV_MENU.map((link) => (
+        <motion.li
+          className="relative h-3/4 flex items-center justify-center"
+          key={link.hash}
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <Link
+            className={clsx(
+              "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+              {
+                "text-[#87B8CE] dark:text-gray-200":
+                  activeSection === link.name,
+              }
+            )}
+            href={link.hash}
+            onClick={() => {
+              setActiveSection(link.name);
+              setTimeOfLastClick(Date.now());
+            }}
+          >
+            {link.name}
+            {link.name === activeSection && (
+              <motion.span
+                className="bg-[#062650] rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                layoutId="activeSection"
+                transition={{
+                  type: "spring",
+                  stiffness: 380,
+                  damping: 30,
+                }}
+              ></motion.span>
+            )}
+          </Link>
+        </motion.li>
+      ))}
+    </ul>
   );
 }
 
@@ -91,26 +122,25 @@ export function Navbar() {
         <MTNavbar
           {...additionalProps}
           blurred
-          placeholder=""
-          className="z-50 mt-6 relative border-0 pr-3 py-3 pl-6 bg-[#87B8CE]"
+          className="z-50 mt-6 border-0 rounded-full relative pr-3 py-3 pl-6 bg-[#87B8CE]"
         >
           <div className="flex items-center justify-between">
-            <Typography {...additionalProps} className="text-lg font-bold text-[#062650]">
+            <Typography
+              {...additionalProps}
+              className="text-lg font-bold text-[#062650]"
+            >
               مدیریت پیشرو هوشمند آرش و آرین
             </Typography>
-            <ul className="ml-10 hidden items-center gap-8 lg:flex">
-              {NAV_MENU.map(({ name }) => (
-                <NavItem key={name} >
-                  {/* // <Icon className="h-5 w-5" /> */}
-                  {name}
-                </NavItem>
-              ))}
-            </ul>
+            {/* <ul className="ml-10 hidden items-center gap-8 lg:flex"> */}
+            {/* {NAV_MENU.map(({ name }) => ( */}
+            <NavItem key={name}>{name}</NavItem>
+            {/* ))} */}
+            {/* </ul> */}
             <div className="hidden items-center gap-4 lg:flex">
-              <Link href="/signin">
+              <Link href="/Login">
                 <Button
                   {...additionalProps}
-                  className="bg-white text-[#062650] rounded-full outline-none focus:scale-110 hover:scale-110"
+                  className="bg-white text-[#062650] text-md rounded-full outline-none focus:scale-110 hover:scale-110"
                 >
                   ورود
                 </Button>
@@ -118,7 +148,7 @@ export function Navbar() {
               <Link href="/contactus">
                 <Button
                   {...additionalProps}
-                  className="rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 bg-[#062650]"
+                  className="rounded-full text-md outline-none focus:scale-110 hover:scale-110 active:scale-105 bg-[#062650]"
                 >
                   دریافت نمایندگی
                 </Button>
@@ -137,27 +167,6 @@ export function Navbar() {
               )}
             </IconButton> */}
           </div>
-          {/* <Collapse open={open}>
-            <div className="container mx-auto mt-3 border-t border-gray-200 px-2 pt-4">
-              <ul className="flex flex-col gap-4">
-                {NAV_MENU.map(({ name, icon: Icon, href }) => (
-                  <NavItem key={name} href={href}>
-                    <Icon className="h-5 w-5" />
-                    {name}
-                  </NavItem>
-                ))}
-              </ul> */}
-          {/* <div className="mt-6 mb-4 flex items-center gap-4">
-                <Button variant="text">Log in</Button>
-                <a
-                  href="https://www.material-tailwind.com/blocks"
-                  target="_blank"
-                >
-                  <Button color="red">blocks</Button>
-                </a>
-              </div> */}
-          {/* </div>
-          </Collapse> */}
         </MTNavbar>
       </div>
     </div>
