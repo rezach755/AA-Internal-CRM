a4e9f287294c1fd398975fc754b2eb706b89003d
rezach755 <reza.ch755@gmail.com>
2024-11-20 09:14:45 +0330

    delete component i dont need

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,4 @@ export * from "../components/hero";
 export * from "./layout";
 export * from "./page";
 export * from "../components/representation-requestform";
-// export * from "../components/explore-courses";
-// export * from "../components/testimonial";
-// export * from "../components/courses-categories";
 export * from "../components/about-us";
